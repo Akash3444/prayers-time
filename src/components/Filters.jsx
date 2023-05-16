ffb20a6a1dff4c35742bd4670a76974131052000
Akash Moradiya <akashmoradiya3444@gmail.com>
2023-05-16 23:03:01 +0530

    Add custom header to the calendar

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,8 +2,24 @@ import React, { useState } from 'react';
 import Spinner from './ui/Spinner/Spinner';
 import momentHijri from 'moment-hijri';
 import ReactDatePicker from 'react-datepicker';
+import moment from 'moment/moment';
+import { getYears } from '../utils/helpers';
 
 const initialHijriYears = ['1441', '1442', '1443', '1444'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 const Filters = ({
   isLoadingEmirates,
   isLoadingAllAreas,
@@ -20,6 +36,7 @@ const Filters = ({
   handleSearchByHijriYear,
 }) => {
   const [years, setYears] = useState(initialHijriYears);
+  const yearList = getYears();
 
   return (
     <div
@@ -100,6 +117,33 @@ const Filters = ({
               }}
               isClearable
               className="w-full rounded-md"
+              renderCustomHeader={({ date, changeYear, changeMonth }) => (
+                <div className="w-full max-w-full flex items-center justify-center gap-2">
+                  <select
+                    value={months[moment(date).month()]}
+                    onChange={({ target: { value } }) => changeMonth(months.indexOf(value))}
+                    className="rounded-md text-sm"
+                  >
+                    {months.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+
+                  <select
+                    value={moment(date).format('YYYY')}
+                    onChange={({ target: { value } }) => changeYear(value)}
+                    className="rounded-md text-sm"
+                  >
+                    {yearList.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              )}
             />
           </div>
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,15 @@ export const axiosInstance = axios.create({
     password: process.env.REACT_APP_API_PASSWORD,
   },
 });
+
+export const getYears = () => {
+  const startYear = 2010;
+  const endYear = 2030;
+  const years = [];
+
+  for (let year = startYear; year <= endYear; year++) {
+    years.push(year);
+  }
+
+  return years;
+};
